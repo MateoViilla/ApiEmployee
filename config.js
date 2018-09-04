3f9d5f76b3fb4d38df9be93aadb79b084dab33a4
MateoViilla <mateoviilla@gmail.com>
2018-09-04 12:08:34 -0500

    ulr mongodb

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3003,
-  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/employeedb'
-  //db: process.env.MONGODB_URI || 'mongodb://admin:adminEmploye3@ds143932.mlab.com:43932/employeedb',
+  //db: process.env.MONGODB_URI || 'mongodb://localhost:27017/employeedb'
+  db: process.env.MONGODB_URI || 'mongodb://admin:adminEmploye3@ds143932.mlab.com:43932/employeedb',
 
 }
